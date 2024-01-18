eae416ff7e590f1cd0dcce47d86b9a4ed9876169
leonardoOliveiraMoura <leonardo.oiveira.moura.perfil@gmail.com>
2024-01-18 15:43:33 -0300

    classes update project

---
+++ recipes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from recipes.views import home 
+from recipes.views import func_contato
+from recipes.views import func_sobre
+
+# hhtp Request
+
+urlpatterns = [
+    path('', home),
+    path('sobre/', func_sobre),
+    path('contato/', func_contato)
+]
---
+++ recipes/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    ...
+    return render(request,'home.html', context={
+        'sendVariable':'informationVariable',
+        })
+    # http response
+
+def func_sobre(request):
+    return HttpResponse('Return of about route')
+    # http response    
+    
+def func_contato(request):
+    return HttpResponse('Return of contact route')
+    # http response    
