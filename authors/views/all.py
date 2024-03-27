598696bc8446a62377ecda5004b70df48419bef3
leonardoOliveiraMoura <leonardo.oiveira.moura.perfil@gmail.com>
2024-03-27 18:33:20 -0300

    crud ok e reorganizacao com estrutura class based views no crud

--- authors/views/all.py
+++ authors/views/all.py
@@ -1,10 +1,11 @@
+from authors.forms import LoginForm, RegisterForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from .forms import LoginForm, RegisterForm
+from recipes.models import Recipe
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
@@ -23,12 +24,9 @@ def register_create(request):
         user.set_password(user.password)
         user.save()
         messages.success(request, 'Your user is created, please log in.')
-
         del(request.session['register_form_data'])
         return redirect(reverse('authors:login'))
-
     return redirect('authors:register')
-
 def login_view(request):
     form = LoginForm()
     return render(request, 'authors/pages/login.html', {
@@ -39,7 +37,6 @@ def login_create(request):
     if not request.POST:
         raise Http404()
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
     if form.is_valid():
         authenticated_user = authenticate(
             username=form.cleaned_data.get('username', ''),
@@ -52,20 +49,48 @@ def login_create(request):
             messages.error(request, 'Invalid credentials')
     else:
         messages.error(request, 'Invalid username or password')
-
-    return redirect(login_url)
-
-
+    return redirect(reverse('authors:dashboard'))
 @login_required(login_url='authors:login', redirect_field_name='next')
 def logout_view(request):
     if not request.POST:
         messages.error(request, 'Invalid logout request')
         return redirect(reverse('authors:login'))
-
     if request.POST.get('username') != request.user.username:
         messages.error(request, 'Invalid logout user')
         return redirect(reverse('authors:login'))
-    
     messages.success(request, 'Logged out successfully')
     logout(request)
     return redirect(reverse('authors:login'))
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user
+    )
+    return render(
+        request,
+        'authors/pages/dashboard.html',
+        context={
+            'recipes': recipes,
+        }
+    )
+
+
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_delete(request):
+    if not request.POST:
+        raise Http404()
+    POST = request.POST
+    id = POST.get('id')
+    recipe = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+        pk=id,
+    ).first()
+    if not recipe:
+        raise Http404()
+    recipe.delete()
+    messages.success(request, 'Deleted successfully.')
+    return redirect(reverse('authors:dashboard'))
